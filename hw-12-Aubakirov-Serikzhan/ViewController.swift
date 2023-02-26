61725fdaea23456d6ed608737accbc5472cb0fef
Serikzhan <serikzhan.aubakirov@gmail.com>
2023-02-26 20:59:18 +0600

    Initial Commit

---
+++ hw-12-Aubakirov-Serikzhan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw-12-Aubakirov-Serikzhan
+//
+//  Created by Serikzhan on 26.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
